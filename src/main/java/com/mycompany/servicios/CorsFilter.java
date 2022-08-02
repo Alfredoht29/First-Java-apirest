010479ef7be05c7bd7367387849ad98d9b67da87
Alfredoht <Emilioaperezvilla@outlook.com>
2022-08-02 09:52:04 -0500

    Cors&Search

---
+++ src/main/java/com/mycompany/servicios/CorsFilter.java
@@ -0,0 +1,23 @@
+
+
+package com.mycompany.servicios;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Especialistaebs2
+ */
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext response) {
+        response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    }
+    
+}
