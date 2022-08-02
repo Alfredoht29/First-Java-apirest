5b44e279de13fbb055f06ff6a6652ee75e09d399
Alfredoht <Emilioaperezvilla@outlook.com>
2022-08-02 09:51:34 -0500

    Cors&Search

--- src/main/java/com/mycompany/servicios/GaleriaServicio.java
+++ src/main/java/com/mycompany/servicios/GaleriaServicio.java
@@ -1,7 +1,9 @@
 package com.mycompany.servicios;
 
+import com.mycompany.dao.GaleriaDao;
 import com.mycompany.dao.PostDao;
 import com.mycompany.modelo.Galeria;
+import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -18,8 +20,9 @@ import javax.ws.rs.core.MediaType;
 public class GaleriaServicio {
 
     private PostDao postDao;
-    public static  Galeria galeriasearch;
-    
+    public static Galeria galeriasearch;
+    private static List<Galeria> galerial = GaleriaDao.getGaleria();
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response doGetResp() {
@@ -32,19 +35,27 @@ public class GaleriaServicio {
     @Produces(MediaType.APPLICATION_JSON)
     public Response createGaleria(Galeria galeria) {
         String titulo;
-        titulo=galeria.getTitulo();
+        titulo = galeria.getTitulo();
         postDao.postGaleria(galeria);
         return Response.ok(titulo).build();
 
     }
+
     @POST
     @Path("/search")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response searchGaleria(Galeria galeria) {
-        galeriasearch=galeria;
-        return Response.ok(galeriasearch).build();
-
+        Galeria x = new Galeria();
+        x.setId(galeria.getId());
+        if (galerial.contains(x)) {
+            for (Galeria obj : galerial) {
+                if (obj.getId() == galeria.getId()) {
+                    return Response.ok(obj).build();
+                }
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 
 }
